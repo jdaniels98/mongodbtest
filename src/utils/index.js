8eea337ddf8c0126d92de694b8e2c9948ddbabc8
jdaniels98 <jackdanielswork98@gmail.com>
2022-10-11 11:35:49 +0100

    Read push

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,7 +4,16 @@ class Movie {
         this.actor = actor
     }
     // write our CRUD operations
+
+    //CREATE
     async create (collection) {
         await collection.insertOne(this)
     }
+
+    //READ
+    async read (collection) {
+        return await collection.find({}).toArray()
+    }
 }
+
+module.exports = Movie
